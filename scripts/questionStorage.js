d2fdbbc10c125afeec5dc20ac6493cc0a4865fca
gemmai95 <gemma.i_95@hotmail.co.uk>
2016-11-25 13:04:27 +0000

    corrections

--- scripts/questionStorage.js
+++ scripts/questionStorage.js
@@ -35,7 +35,7 @@ function textInit(Qnum){
 	}
 }
 function removeStorage(noInput){
- window.alert("done");
+ //window.alert("done");
 	for(i=0;i<4;i++){
 		if(sessionStorage.getItem('Response1' + i) != null){
 			sessionStorage.removeItem('Response1' + i);
